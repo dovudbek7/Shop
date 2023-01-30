c7fb163968085755c5514421ccb4e5c018164d9b
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-01-30 16:16:52 +0500

    new

--- landing_page/landing_page.js
+++ landing_page/landing_page.js
@@ -1,1 +1,38 @@
-
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+      loop:true,
+    grabCursor:true,
+    spaceBetween:20,
+    
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // breakpoints: {
+    //     0: {
+    //         slidesPerView: 1,
+    //     },
+    //     100 : {
+    //         slidesPerView: 2,
+    //     },
+    //     991: {
+    //         slidesPerView: 3,
+    //     },
+    // },
+  });
+  
